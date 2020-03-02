b1bc0eb23a520cbaadd2fc9ad1a03e9dcd50c616
cedric-winbush <cawinbushjr@gmail.com>
2020-03-02 13:52:31 -0600

    add login page

---
+++ client/src/components/Login.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Login = () => {
+  const history = useHistory();
+
+  const [ login, setLogin ] = ({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setLogin({
+      ...login, 
+      [e.target.name ]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('what are we posting to login: ', login)
+    axiosWithAuth()
+      .post('/login', login)
+      .then(res => {
+        console.log('this is the response from post: ', res);
+        window.localStorage.setItem('token', res.data.payload);
+        history.push('/dashboard');
+      })
+      .catch(err => {
+        console.log('there was an error with login: ', err)
+      })
+
+  }
+  return (
+    <div>
+			<form className='' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='username'
+          name='username'
+          value={login.username}
+          label='username'
+          onChange={handleChange}
+          className='input'
+        />
+        <input
+          type='password'
+          placeholder='password'
+          name='password'
+          value={login.password}
+          label='password'
+          onChange={handleChange}
+          className=''
+        />
+        <button className=''>Login</button>
+      </form>
+		</div>
+  );
+};
+
+export default Login;
+
+
+}
+
