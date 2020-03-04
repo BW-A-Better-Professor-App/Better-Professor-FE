0bd168ab7f5e032b4d163d03e92de8b85c3ce2b4
ranccm <ranimnapier@gmail.com>
2020-03-03 16:52:40 -0800

    deadlines component

---
+++ client/src/components/StudentCard.js
@@ -0,0 +1,7 @@
+import React from  'react';
+
+export default function StudentCard(props){
+    return(
+        <h2>{props.name}</h2>
+    );
+};
--- client/src/components/UpcomingDeadlines.js
+++ client/src/components/UpcomingDeadlines.js
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth'; 
 
 export default function UpcomingDeadlines() {
+        const [deadlines, setDeadlines] = useState([])
+
+        useEffect(() => {
+            axiosWithAuth()
+            .get(`/tasks`)
+            .then(response => {
+                setDeadlines(resonse.data);
+            })
+            .catch(err => {
+                console.log('unable to fetch deadlines', err);
+            })
+        }, []);
+
     return(
-        <div>deadlines</div>
+        <div className='upcoming-deadlines'>
+            {deadlines.map(deadline => (
+                <div key={deadline.id}>
+                    <h3>{deadline.name}</h3>
+                    <p>{deadline.duedate}</p>
+                </div>
+            ))}
+
+        </div>
     )
 }
