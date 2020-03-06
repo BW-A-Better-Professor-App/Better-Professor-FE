73064013bc265ac81b082f125858a27351f9c484
nightsrfr <ditonp@gmail.com>
2020-03-06 09:59:47 -0500

    login page styled

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import styled from 'styled-components'
+
 
 const Login = () => {
   const history = useHistory();
@@ -36,7 +38,7 @@ const Login = () => {
 
   }
   return (
-    <div>
+    <Container>
 			<form className='' onSubmit={handleSubmit}>
         <input
           type='text'
@@ -61,10 +63,84 @@ const Login = () => {
           <Link to= '/Login/Student' className="FormField_Link">Not a Teacher? Login As Student Here</Link>
         </div>
       </form>
-		</div>
+      </Container>
   );
 };
 
+const Container = styled.div`
+    color: #444444;
+    h3 {
+        font-size: 2.5rem;
+        font-weight: 700;
+        letter-spacing: 0.1rem;
+        padding-bottom: 1rem;
+        border-bottom: 1px dotted #444444;
+        margin-bottom: 5%;
+        text-align: center;
+        @media (max-width: 400px) {
+            font-size: 2rem;
+        }
+    }
+    
+    /* Registration form styling */
+    form {
+        padding: 2.5rem 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        /* Input styling */
+        input {
+            margin: 0.5rem 0;
+            width: 20rem;
+            height: 3.5rem;
+            background: #bfbfbf;
+            border: none;
+            border-radius: 0.3rem;
+            padding: 0.5rem 0.5rem 0.5rem 1rem;
+            font-size: 1.2rem;
+            font-weight: 300;
+            letter-spacing: 0.1rem;
+        
+            &:focus {
+                outline: none;
+                border: 1px solid #ababab;
+            }
+        }
+        
+        /* Button Styling */
+        button {
+            width: 20rem;
+            height: 3.5rem;
+            margin: 1rem 0;
+            background: #d1ffd6;
+            border: none;
+            border-radius: 0.3rem;
+            transition: all 100ms;
+            box-shadow: 0px 2px 5px -5px;
+            letter-spacing: 0.1rem;
+        
+            &:hover {
+                transition: background 100ms;
+                cursor: pointer;
+                background: #afdeb4;
+            }
+        }
+        /* Login if already registered */
+        .extra-options {
+            margin: 1rem 0;
+            width: 80%;
+            display: flex;
+            justify-content: center;
+            span {
+                color: black;
+                &:hover {
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+`;
 export default Login;
 
 
